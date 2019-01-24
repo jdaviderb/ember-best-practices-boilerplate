fa731a14f32d3172367e56fe89cb53a8d10adf1d
Jorge Hernandez <jdaviderb@gmail.com>
2019-01-24 15:52:44 -0500

    added Ember FastBoot App Server

--- config/environment.js
+++ config/environment.js
@@ -23,7 +23,7 @@ module.exports = function(environment) {
     },
 
     fastboot: {
-      hostWhitelist: [/^localhost:\d+$/]
+      hostWhitelist: ['ember-bests-practices.herokuapp.com', /^localhost:\d+$/]
     }
   };
 
