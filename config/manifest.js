4cf715940521c06d63f985c16a9ddccc3dd2138f
Jorge Hernandez <jdaviderb@gmail.com>
2019-01-24 15:26:25 -0500

    added addon ember-service-worker

---
+++ config/manifest.js
@@ -0,0 +1,19 @@
+module.exports = function(/* environment, appConfig */) {
+  // See https://github.com/zonkyio/ember-web-app#documentation for a list of
+  // supported properties
+
+  return {
+    name: "Ember Bests Practices",
+    short_name: "myscraping",
+    description: "",
+    start_url: "/",
+    display: "standalone",
+    background_color: "#fff",
+    theme_color: "#fff",
+    icons: [
+    ],
+    ms: {
+      tileColor: '#fff'
+    }
+  };
+}
