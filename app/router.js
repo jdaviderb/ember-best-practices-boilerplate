64787f71367f06a9bf5c2581cb4b4b2ed5a8c9de
Beto Cantu <betocantu93@gmail.com>
2019-01-24 22:58:02 -0600

    add prember, prember-middleware, ember-service-worker-prember

--- app/router.js
+++ app/router.js
@@ -6,6 +6,8 @@ const Router = EmberRouter.extend({
   rootURL: config.rootURL
 });
 
-Router.map(function() {});
+Router.map(function() {
+  this.route('about');
+});
 
 export default Router;
--- ember-cli-build.js
+++ ember-cli-build.js
@@ -4,7 +4,12 @@ const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function(defaults) {
   let app = new EmberApp(defaults, {
-    // Add options here
+    prember: {
+      urls: [
+        '/',
+        '/about'
+      ]
+    }
   });
 
   // Use `app.import` to add additional libraries to the generated
