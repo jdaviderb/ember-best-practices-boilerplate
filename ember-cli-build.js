6614bbd95380aac1eac8e4354f7cba33886058c8
Beto Cantu <betocantu93@gmail.com>
2019-01-25 00:02:52 -0600

    prettier

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -3,9 +3,7 @@
 const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function(defaults) {
-  let app = new EmberApp(defaults, {
-    
-  });
+  let app = new EmberApp(defaults, {});
 
   // Use `app.import` to add additional libraries to the generated
   // output files.
