4b656d50f8179156474af25fa2e85b99652efe78
Jorge Hernandez <jdaviderb@gmail.com>
2019-01-26 15:09:51 -0500

    add test coverage 100% (#12)

---
+++ tests/acceptance/home-test.js
@@ -0,0 +1,14 @@
+import { module, test } from 'qunit';
+import { visit, currentURL } from '@ember/test-helpers';
+import { setupApplicationTest } from 'ember-qunit';
+
+module('Acceptance | home', function(hooks) {
+  setupApplicationTest(hooks);
+
+  test('visiting /', async function(assert) {
+    await visit('/');
+
+    assert.equal(currentURL(), '/');
+    assert.dom('h1').hasText('hello world');
+  });
+});
